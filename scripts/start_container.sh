6349383f7b59f3f9b1598ebafe481f6ca1d40092
Mohitkr0621 <88219453+Mohitkr0621@users.noreply.github.com>
2024-06-14 19:19:09 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull mohit0621/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 mohit0621/simple-python-flask-app
+docker run -d -p 6000:6000 mohit0621/simple-python-flask-app
